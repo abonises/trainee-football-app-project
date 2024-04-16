7a718f6fce48b56934d65f5700d874b62f821ded
Sashas <sasaspychka@gmail.com>
2024-04-16 23:57:30 +0300

    second-commit

--- data/data.ts
+++
@@ -1,6 +0,0 @@
-export const TEAM_URL_ONE = 'http://localhost:4000/response'
-export const TEAM_URL_TWO = 'http://localhost:5000/response'
-export const TEAM_URL_THREE = 'http://localhost:5001/response'
-
-
-export const arrayTeams = ['team1', 'team2', 'team3']
